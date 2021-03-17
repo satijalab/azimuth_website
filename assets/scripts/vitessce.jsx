34043bccbea56c2944c224f7e92f76950c8d367e
Jaison Jain <jaisonj708@gmail.com>
2021-03-17 02:52:16 -0400

    update

--- assets/scripts/vitessce.jsx
+++ assets/scripts/vitessce.jsx
@@ -1,16 +1,62 @@
-import myViewConfig from './static/json/human_motor_cortex.json';
-// const myViewConfig = require('./static/json/human_motor_cortex.json');
-import * as vitessce from 'vitessce'
+// import myViewConfig from './static/json/human_motor_cortex.json';
+// // const myViewConfig = require('./static/json/human_motor_cortex.json');
+// import * as vitessce from 'vitessce'
+
+// export default function VitessceApp() {
+//     return (
+//         <vitessce.Vitessce
+//             config={myViewConfig}
+//             height={800}
+//             theme="dark"
+//         />
+//     );
+// }
+
+// ReactDOM.render(React.createElement(VitessceApp),
+//     document.getElementById("vitessce"));
+
+
+
+import { Scatterplot } from 'vitessce/es/production/scatterplot.min.js';
+import { Status } from 'vitessce/es/production/status.min.js';
+import 'vitessce/es/production/static/css/index.css';
 
 export default function VitessceApp() {
+    const view = { target: [0, 0, 0], zoom: 0.75 };
+    const mapping = "PCA";
+    const cells = {
+        1: { mappings: { [mapping]: [0, 0] } },
+        2: { mappings: { [mapping]: [1, 1] } },
+        3: { mappings: { [mapping]: [1, 2] } }
+    };
+    const selectedCellIds = new Set();
+    const dimensions = { width: '400px', height: '400px', margin: '10px' };
+
     return (
-        <vitessce.Vitessce
-            config={myViewConfig}
-            height={800}
-            theme="dark"
-        />
+        <div className="vitessce-container vitessce-theme-light">
+            <div className="card card-body bg-secondary" style={dimensions}>
+                <Status
+                    info="Hello world"
+                    removeGridComponent={() => {}}
+                />
+            </div>
+            <div className="card card-body bg-secondary" style={dimensions}>
+                <Scatterplot
+                    uuid="my-vitessce-scatterplot"
+                    view={view}
+                    mapping={mapping}
+                    cells={cells}
+                    selectedCellIds={selectedCellIds}
+                    cellColors={null}
+                    updateStatus={(message) => {}}
+                    updateCellsSelection={(selectedIds) => {}}
+                    updateCellsHover={(hoverInfo) => {}}
+                    updateViewInfo={(viewInfo) => {}}
+                    clearPleaseWait={(layerName) => {}}
+                />
+            </div>
+        </div>
     );
 }
 
-ReactDOM.render(React.createElement(VitessceApp),
-    document.getElementById("vitessce"));
+ReactDOM.render(React.createElement(VitessceApp), document.getElementById("vitessce"));
