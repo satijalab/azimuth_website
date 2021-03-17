55e2875269a997dab080cca83e55fa94540c8af2
Jaison Jain <jaisonj708@gmail.com>
2021-03-17 03:24:47 -0400

    update

--- assets/scripts/vitessce.jsx
+++ assets/scripts/vitessce.jsx
@@ -3,15 +3,44 @@
 const myViewConfig = require('./static/json/human_motor_cortex.json');
 
 export default function VitessceApp() {
-    return (
-        <div>
-          Hi Hi Hi
-        </div>
-    );
+  const view = { target: [0, 0, 0], zoom: 0.75 };
+  const mapping = "PCA";
+  const cells = {
+    1: { mappings: { [mapping]: [0, 0] } },
+    2: { mappings: { [mapping]: [1, 1] } },
+    3: { mappings: { [mapping]: [1, 2] } }
+  };
+  const selectedCellIds = new Set();
+  const dimensions = { width: '400px', height: '400px', margin: '10px' };
+  return (
+    <div className="vitessce-container vitessce-theme-light">
+      <div className="card card-body bg-secondary" style={dimensions}>
+        <Status
+          info="Hello world"
+          removeGridComponent={() => { }}
+        />
+      </div>
+      <div className="card card-body bg-secondary" style={dimensions}>
+        <Scatterplot
+          uuid="my-vitessce-scatterplot"
+          view={view}
+          mapping={mapping}
+          cells={cells}
+          selectedCellIds={selectedCellIds}
+          cellColors={null}
+          updateStatus={(message) => { }}
+          updateCellsSelection={(selectedIds) => { }}
+          updateCellsHover={(hoverInfo) => { }}
+          updateViewInfo={(viewInfo) => { }}
+          clearPleaseWait={(layerName) => { }}
+        />
+      </div>
+    </div>
+  );
 }
 
 ReactDOM.render(React.createElement(VitessceApp),
-    document.getElementById("vitessce"));
+  document.getElementById("vitessce"));
 
 
 // function Example() {
