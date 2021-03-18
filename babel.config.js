b2b413725397f82aa98fb6b51d6c4725aae9ce9d
Jaison Jain <jaisonj708@gmail.com>
2021-03-17 21:24:52 -0400

    update

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,9 @@
-
 module.exports = function (api) {
   api.cache(true);
   const presets = [
     ["@babel/preset-react"]
     ]
   const plugins = [
-    "@babel/plugin-syntax-jsx",
-    ["transform-inline-environment-variables", {
-      "include": [
-        "NODE_ENV"
-      ]
-    }]
   ];
   return {
     presets,
